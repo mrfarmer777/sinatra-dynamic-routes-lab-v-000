b9b5114ad439d5bb296b43e461bb282adae9708b
Matt Farmer <mrfarmer777@gmail.com>
2018-06-27 20:10:32 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,13 +14,13 @@ class App < Sinatra::Base
     (num*num).to_s
   end
 
-  get 'say/:number/:phrase' do
+  get '/say/:number/:phrase' do
     num=params[:number].to_i
     phrase=params[:phrase]
     "#{phrase}"
   end
 
-  get 'say/:word1/:word2/:word3/:word4/:word5' do
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}"
   end
 
@@ -40,6 +40,5 @@ class App < Sinatra::Base
     else
       'Invalid operationsss'
     end
-
   end
 end
